c75c4c0274928720f7ac567ca8f04055092a4027
jaydeeptrivedi13 <jaydeep.trivedi@wisdmlabs.com>
2025-04-01 13:17:05 +0530

    Updated version

--- edwiserbridge/version.php
+++ edwiserbridge/version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025040200;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '4.0.0';
+$plugin->version   = 2025040201;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '4.0.1';
 $plugin->requires  = 2023100900; // Requires this Moodle version (Moodle V4.3).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'auth_edwiserbridge'; // Full name of the plugin (used for diagnostics).
