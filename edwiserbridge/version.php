3b9dae84c9cf7bdbe8912db328a875dc3241b525
UjjawalWisdmlabs <ujjawal.srivastava@wisdmlabs.com>
2025-09-05 13:52:56 +0530

    Updated version and Changes.txt files

--- edwiserbridge/version.php
+++ edwiserbridge/version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025060900;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '4.1.0';
+$plugin->version   = 2025090800;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '4.3.0';
 // $plugin->requires  = 2023100900; // Requires this Moodle version (Moodle V4.3).
 $plugin->requires  = 2020061500; // Requires this Moodle version (Moodle V3.9).
 $plugin->maturity  = MATURITY_STABLE;
